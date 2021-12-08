c6fb83823ae96d77391e8f23da5d4c4d4a5a138a
Jiro13 <95729392+Jiro13@users.noreply.github.com>
2021-12-07 22:07:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('ya lo sabia bby ya pero serio me confirmas al whats')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
